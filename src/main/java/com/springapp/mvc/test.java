a5ced5d334c93c906c32a40ef422f8c0d2294d1a
juddammu <juddammus@gmail.com>
2013-08-02 13:57:19 +0900

    test

---
+++ src/main/java/com/springapp/mvc/test.java
@@ -0,0 +1,12 @@
+package com.springapp.mvc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13. 8. 2
+ * Time: 오후 1:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+
+}
